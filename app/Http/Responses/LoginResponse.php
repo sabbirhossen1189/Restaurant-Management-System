24516c33569b5b85dc7cd9c3897c701d1d64f179
moshiurkn <rahaman2305341338@diu.edu.bd>
2026-03-04 03:12:47 +0600

    this feature was made by Moshiur (Step 2: Implement role-based LoginResponse)

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Illuminate\Support\Facades\Auth;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        $usertype = Auth::user()->usertype;
+
+        if ($usertype === 'admin') {
+            return redirect()->to('/admin/dashboard');
+        }
+
+        return redirect()->to('/home');
+    }
+}
